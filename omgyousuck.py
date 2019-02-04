8b7836316b016b7f161990df0ca355d4f721d4fb
ehauckdo <eduardohauck@gmail.com>
2019-02-04 13:44:37 +0900

    commenting and cleaning code

--- omgyousuck.py
+++ omgyousuck.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division
 
-# Sample random python-based agent for AIWolf
+# Sample python-based agent for AIWolf
+# Implement simple heuristics when playing the WEREWOLF role
+# For other roles, just play randomly
 # Based on https://github.com/k-harada/AIWolfPy
 # Developed for aiwolf 0.4.12 (2018/06/16)
 # Author: ehauckdo
@@ -20,26 +22,25 @@ class SampleAgent(object):
 
 	def __init__(self, agent_name):
 		self.myname = agent_name
-		self.sleeptime = 0.1
-
-	def getName(self):
-		return self.myname
 
 	def initialize(self, base_info, diff_data, game_setting):
 		self.id = base_info["agentIdx"]
 		self.base_info = base_info
 		self.game_setting = game_setting
 
-		self.game_history = {}
-		self.player_map = {}
-		self.target_list = []
+		self.game_history = {} # stores each sentence stated from each day
+		self.player_map = {}   # a map with the status and other info for each player
+		self.target_list = []  # a queue storing possible targets to act against
 		self.current_target = None
 
 		printGameSetting(game_setting)
 		self.updatePlayerMap(base_info)
 
+	def getName(self):
+		return self.myname
+
 	def update(self, base_info, diff_data, request):
-		print(getTimeStamp()+" inside Update")
+		print("Executing update...")
 		self.base_info = base_info
 		
 		printBaseInfo(base_info)
@@ -49,7 +50,7 @@ class SampleAgent(object):
 		self.updatePlayerMap(base_info)
 
 	def dayStart(self):
-		print(getTimeStamp()+" inside dayStart")
+		print("Executing dayStart...")
 
 		# at the start of each day, either assign a new random target
 		# or fetch a new target from the target list (agents who previously voted against us)
@@ -58,29 +59,27 @@ class SampleAgent(object):
 			while len(self.target_list) > 0:
 				selected = self.target_list.pop(0)
 				if self.player_map[selected]["status"] != "DEAD":
-					print("Selecting new target from target_list... Selected ID: "+str(selected))
+					print("Selected new target from target_list, ID: "+str(selected))
 					self.setTarget(selected, True)
 					break
-				else:
-					continue
+			# if there are no agents in the target list or they are already dead,
+			# target a new random agent
 			else:
 				selected = randomPlayerId(self.base_info)
 				print("Selecting a random target... Selected ID: "+str(selected))
 				self.setTarget(selected, False)
 
-		return None
-
 	def talk(self):
-		print(getTimeStamp()+" inside Talk")
+		print("Executing talk...")
 		
 		if self.player_map[self.current_target]["revenge"] is False: 
 			print("Voting on random target")
 		else:
 			print("Voting for revenge!")
 
-		# Talking against a target has 3 steps: first estimate,
-		# then state your vote, then start requesting other agents
-		# to vote against target
+		# Talking against a target has 3 steps: (0) first estimate,
+		# then (1) state your vote, then (>2) start requesting other 
+		# agents to vote against the current target
 		if self.player_map[self.current_target]["targetStatus"] == 0: 	
 			talk = cb.estimate(self.current_target, "WEREWOLF")
 		elif self.player_map[self.current_target]["targetStatus"] == 1:
@@ -92,46 +91,45 @@ class SampleAgent(object):
 		return talk
 
 	def whisper(self):
-		print(getTimeStamp()+" inside Whisper")
+		print("Executing whisper...")
 		if self.current_target != None:
-			print("Whispering request against current target: ")
 			selected = self.current_target
+			print("Whispering request against current target: "+str(selected))
 		else:
 			selected = randomPlayerId(self.base_info)
 			print("Whispering request against random agent: "+str(selected))
 		return cb.request(cb.attack(selected))
 
 	def vote(self):
-		print(getTimeStamp()+" inside Vote")
+		print("Executing vote...")
 		if self.current_target != None:
-			print("Voting on current target: ")
-			return self.current_target
+			selected = self.current_target
+			print("Voting on current target: "+str(selected))
 		else:
 			selected = randomPlayerId(self.base_info)
 			print("Voting on random agent: "+str(selected))
-			return selected
+		return selected
 
 	def attack(self):
-		print(getTimeStamp()+" inside Attack")
+		print("Executing attack...")
 		if self.current_target != None:
-			print("Attacking current target: ")
-			return self.current_target
+			selected = self.current_target
+			print("Attacking current target: "+str(selected))
 		else:
 			selected = randomPlayerId(self.base_info)
 			print("Attacking random agent: "+str(selected))
-			return selected
+		return selected
 		
 	def divine(self):
-		print(getTimeStamp()+" inside Divine")
+		print("Executing divine randomly...")
 		return randomPlayerId(self.base_info)
 
 	def guard(self):
-		print(getTimeStamp()+" inside Guard")
+		print("Executing guard randomly...")
 		return randomPlayerId(self.base_info)
 	
 	def finish(self):
-		print(getTimeStamp()+" inside Finish")
-		return None
+		print("Executing finish...")
 
 	def updatePlayerMap(self, base_info):
 		if self.player_map == None:
@@ -148,7 +146,6 @@ class SampleAgent(object):
 				self.player_map[agent_id]["whispered"] = False
 
 	def updateGameHistory(self, diff_data):
-
 		for row in diff_data.itertuples():
 
 			current_day = getattr(row, "day")
@@ -167,26 +164,29 @@ class SampleAgent(object):
 			if "{:02d}".format(self.id) in text:
 				if "ESTIMATE" in text or "VOTE" in text or ("DIVINED" in text and "WEREWOLF" in text):
 					
-					# if we are pursuing revenge against someone already, add this 
-					# new agent to the target liost
+					# if we are pursuing revenge against someone already  
+					# add this new agent to the target list
 					if self.player_map[self.current_target]["revenge"] == True:
 						self.target_list.append(agent)
 					# otherwise, set this new agent as the current target
 					else:
 						self.setTarget(agent, True)
 						
-
+	# Set some agent as the current target. Revenge param says 
+	# whether or not this agent acted against us before
 	def setTarget(self, id, revenge):
 		self.current_target = id
 
 		self.player_map[id]["revenge"] = revenge
 		self.player_map[id]["targetStatus"] = 0
 
+
 def parseArgs(args):
 	usage = "usage: %prog [options]"
 	parser = optparse.OptionParser(usage=usage) 
 
-	# need this to ensure -h can be used as an option
+	# need this to ensure -h (for hostname) can be used as an option 
+	# in optparse before passing the arguments to aiwolfpy
 	parser.set_conflict_handler("resolve")
 
 	parser.add_option('-h', action="store", type="string", dest="hostname",
